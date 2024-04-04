3e7e68c5ac69bb64a63ab3fb3eb9f413cbc10b30
neruneruna7 <neruneruna7@gmail.com>
2024-04-04 18:44:56 +0900

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,29 +1,43 @@
 #!/usr/bin/env bash
+# スクリプトがエラーに遭遇したときに終了し、未定義の変数があるとエラーを出力します。
 set -ue
 
+# ヘルプメッセージを表示する関数
 helpmsg() {
   command echo "Usage: $0 [--help | -h]" 0>&2
   command echo ""
 }
 
+# ホームディレクトリにドットファイルをリンクする関数
 link_to_homedir() {
   command echo "backup old dotfiles..."
+
+  # バックアップディレクトリが存在しない場合、作成します。
   if [ ! -d "$HOME/.dotbackup" ];then
     command echo "$HOME/.dotbackup not found. Auto Make it"
     command mkdir "$HOME/.dotbackup"
   fi
 
+  # スクリプトのディレクトリを取得します。
   local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+
+  # ドットファイルのディレクトリを取得します。
   local dotdir=$(dirname ${script_dir})
+
+  # ホームディレクトリとドットファイルのディレクトリが異なる場合、リンクを作成します。
   if [[ "$HOME" != "$dotdir" ]];then
     for f in $dotdir/.??*; do
+      # .gitは除外します。
       [[ `basename $f` == ".git" ]] && continue
+      # 既存のリンクがある場合、削除します。
       if [[ -L "$HOME/`basename $f`" ]];then
         command rm -f "$HOME/`basename $f`"
       fi
+      # 既存のファイルがある場合、バックアップします。
       if [[ -e "$HOME/`basename $f`" ]];then
         command mv "$HOME/`basename $f`" "$HOME/.dotbackup"
       fi
+      # 新しいリンクを作成します。
       command ln -snf $f $HOME
     done
   else
@@ -31,12 +45,15 @@ link_to_homedir() {
   fi
 }
 
+ # コマンドライン引数を解析します。
 while [ $# -gt 0 ];do
   case ${1} in
     --debug|-d)
+      # デバッグモードを有効にします。
       set -uex
       ;;
     --help|-h)
+      # ヘルプメッセージを表示します。 
       helpmsg
       exit 1
       ;;
@@ -46,6 +63,7 @@ while [ $# -gt 0 ];do
   shift
 done
 
+# ドットファイルのリンクを作成します。
 link_to_homedir
 git config --global include.path "~/.gitconfig_shared"
 command echo -e "\e[1;36m Install completed!!!! \e[m"
