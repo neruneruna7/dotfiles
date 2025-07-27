c8a8aeb0688caa083051bcffc140640b27b2d566
neruneruna7 <neruneruna7@gmail.com>
2025-07-28 07:46:45 +0900

    u

--- nushell/setup.sh
+++ nushell/setup.sh
@@ -6,3 +6,7 @@ mkdir -p ~/.config/nushell
 ln -sf ~/dotfiles/nushell/config.nu ~/.config/nushell/config.nu
 ln -sf ~/dotfiles/nushell/env.nu ~/.config/nushell/env.nu
 # ...existing code...
+
+# こっちのが新しいっぽい？
+ln -sf ~/dotfiles/nushell/config.nu ~/Library/Application\ Support/nushell/config.nu
+ln -sf ~/dotfiles/nushell/env.nu ~/Library/Application\ Support/nushell/env.nu
--- scripts/mac/brew.sh
+++ scripts/mac/brew.sh
@@ -2,3 +2,7 @@
 brew install starship
 # ターミナル
 brew install nushell
+brew install zoxide
+brew install fzf
+brew install mold
+brew install sccache
